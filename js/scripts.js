1c06c9aeedba1101724cef7a568073e9ba1be479
Iader Folli <iader_93@hotmail.it>
2022-12-20 18:47:05 +0100

    agg

--- js/scripts.js
+++ js/scripts.js
@@ -13,40 +13,52 @@ const submitGenera = document.getElementById("genera");
 // BUTTON ANNULLA
 const submitAnnulla = document.getElementById("annulla");
 
-// CALCOLA PREZZO X KM
-let prezzo = numKm * 0.21;
-console.log ("Il prezzo è " + prezzo + " € ");
-
 
 submitGenera.addEventListener('click',
-    function() {
+    function () {
+        // CALCOLA PREZZO X KM        
+        const kmVal = parseInt(numKm.value);
+        let prezzo = kmVal * 0.21;
+        console.log("Il prezzo è " + prezzo + " € ");
         console.log('Cliccato su submitGenera');
-        console.log('Il valore di nameInput e', nameInput.value);
-        console.log('Il valore di numKm e', numKm.value);
-        console.log('Il valore di eta e', eta.value);
+        console.log('Il valore di nameInput è', nameInput.value);
+        console.log('Il valore di numKm è', kmVal);
+        console.log('Il valore di eta è', eta.value);
+
+
+        // SE MINORE DI 18 ANNI APPLICA SCONTO 20%
+        if (eta.value == 'under18') {
+            prezzo = prezzo * 0.8;
+            console.log("Lo sconto del 20% è stato applicato: " + prezzo + " € ");
+        }
+
+        // SE MAGGIORE DI 65 ANNI APPLICA SCONTO 40%
+        else if (eta.value == 'over65') {
+            prezzo = prezzo * 0.6;
+            console.log("Lo sconto del 40% è stato applicato: " + prezzo + " € ");
+        }
+
+        // PREZZO CON 2 VALORI DECIMALI
+        prezzo = prezzo.toFixed(2)
+        console.log("Prezzo finale con 2 decimali: " + prezzo + " € ");
+
+
+        const outputName = document.getElementById("nome-passeggero");
+        outputName.innerHTML = nameInput.value;
+
+        const outputAge = document.getElementById("eta-passeggero");
+        outputAge.innerHTML = eta.value;
+
+    }
+)
+submitAnnulla.addEventListener('click',
+    function () {
+        // nameInput.value = "";
+        // numKm.value = "";
+        // eta.value = "over18";
+        window.location.reload();
     }
 )
 
-// SE MINORE DI 18 ANNI APPLICA SCONTO 20%
-if (eta < 18) {
-    prezzo = prezzo * 0.8;
-    console.log("Lo sconto del 20% è stato applicato: " + prezzo + " € ");
-} 
-
-// SE MAGGIORE DI 65 ANNI APPLICA SCONTO 40%
-else if (eta >= 65) {
-    prezzo = prezzo * 0.6;
-    console.log("Lo sconto del 40% è stato applicato: " + prezzo + " € ");
-}
-
-// PREZZO CON 2 VALORI DECIMALI
-prezzo = prezzo.toFixed(2)
-console.log("Prezzo finale con 2 decimali: " + prezzo + " € ");
-
-
 // //STAMPA PREZZO IN HTML
 // alert("Il prezzo finale del biglietto è: " + prezzo + " € ")
-
-
-
-

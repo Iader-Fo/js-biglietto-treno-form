493948be9ce3c30c6cf980d9baf912c09fef61c6
Iader Folli <iader_93@hotmail.it>
2022-12-20 20:14:34 +0100

    aggiornamento JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // NOME E COGNOME
 const nameInput = document.getElementById("name-input");
+console.log("Il nome e cognome è:", nameInput)
 
 // CHIEDI N. DI KM
 const numKm = document.getElementById("numkm");
@@ -13,12 +14,20 @@ const submitGenera = document.getElementById("genera");
 // BUTTON ANNULLA
 const submitAnnulla = document.getElementById("annulla");
 
+// nameInput.addEventListener('change',
+// function (ev) {
+//     console.log (ev)
+// }
+// )
 
 submitGenera.addEventListener('click',
     function () {
-        // CALCOLA PREZZO X KM        
+        // CALCOLA PREZZO X KM
+        console.log ("i km sono:", numKm);        
         const kmVal = parseInt(numKm.value);
         let prezzo = kmVal * 0.21;
+        let bigliettoStandard = "Biglietto Standard"
+
         console.log("Il prezzo è " + prezzo + " € ");
         console.log('Cliccato su submitGenera');
         console.log('Il valore di nameInput è', nameInput.value);
@@ -29,12 +38,14 @@ submitGenera.addEventListener('click',
         // SE MINORE DI 18 ANNI APPLICA SCONTO 20%
         if (eta.value == 'under18') {
             prezzo = prezzo * 0.8;
+            bigliettoStandard = "Biglietto ridotto"
             console.log("Lo sconto del 20% è stato applicato: " + prezzo + " € ");
         }
 
         // SE MAGGIORE DI 65 ANNI APPLICA SCONTO 40%
         else if (eta.value == 'over65') {
             prezzo = prezzo * 0.6;
+            bigliettoStandard = "Biglietto ridotto"
             console.log("Lo sconto del 40% è stato applicato: " + prezzo + " € ");
         }
 
@@ -47,10 +58,21 @@ submitGenera.addEventListener('click',
         outputName.innerHTML = nameInput.value;
 
         const outputAge = document.getElementById("eta-passeggero");
-        outputAge.innerHTML = eta.value;
+        outputAge.innerHTML = bigliettoStandard;
+
+        const outputCarrozza = document.getElementById("num-carrozza");
+        outputCarrozza.innerHTML = Math.floor(Math.random() * 10 % 11 + 1);
+
+        const outputCodiceCp = document.getElementById("cod-cp");
+        outputCodiceCp.innerHTML = Math.floor(Math.random() * (99999 - 90000 + 1) + 90000);
+
+        const outputCostoBiglietto = document.getElementById("costo-biglietto");
+        outputCostoBiglietto.innerHTML = prezzo + " €";
 
     }
 )
+
+
 submitAnnulla.addEventListener('click',
     function () {
         // nameInput.value = "";
@@ -59,6 +81,3 @@ submitAnnulla.addEventListener('click',
         window.location.reload();
     }
 )
-
-// //STAMPA PREZZO IN HTML
-// alert("Il prezzo finale del biglietto è: " + prezzo + " € ")

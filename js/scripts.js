8afae92269ab7249c1305ff99029ed91b1ea967c
Iader Folli <iader_93@hotmail.it>
2022-12-20 16:23:33 +0100

    Inizializzazione

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// // CHIEDI N. DI KM
+// //parseInt p vedere il valore in numero
+// const numkm = parseInt(prompt("Quanti chilometri desidera percorrere?"));
+// console.log ("Il passeggero vuole percorrere " + numkm + " km ");
+
+// // CHIEDI ETA'
+// const eta = parseInt(prompt( "Quanti anni ha?"));
+// console.log ("Il passeggero ha " + eta + " anni ");
+
+// // CALCOLA PREZZO X KM
+// let prezzo = numkm * 0.21;
+// console.log ("Il prezzo è " + prezzo + " € ");
+
+// // SE MINORE DI 18 ANNI APPLICA SCONTO 20%
+// if (eta < 18) {
+//     prezzo = prezzo * 0.8;
+//     console.log("Lo sconto del 20% è stato applicato: " + prezzo + " € ");
+// } 
+
+// // SE MAGGIORE DI 65 ANNI APPLICA SCONTO 40%
+// else if (eta >= 65) {
+//     prezzo = prezzo * 0.6;
+//     console.log("Lo sconto del 40% è stato applicato: " + prezzo + " € ");
+// }
+
+// // PREZZO CON 2 VALORI DECIMALI
+// prezzo = prezzo.toFixed(2)
+// console.log("Prezzo finale con 2 decimali: " + prezzo + " € ");
+
+
+// //STAMPA PREZZO IN HTML
+// alert("Il prezzo finale del biglietto è: " + prezzo + " € ")
+
+
+
+
